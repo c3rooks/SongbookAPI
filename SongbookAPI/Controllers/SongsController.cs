00a87fe2f73d30700b05782fe599f1d961cd2ecf
Corey Crooks <corey@crooksconsulting.com>
2023-05-28 10:12:59 -0400

    updates to songcontroller

--- SongbookAPI/Controllers/SongsController.cs
+++ SongbookAPI/Controllers/SongsController.cs
@@ -18,6 +18,21 @@ public class SongsController : ControllerBase
         _songs = _database.GetCollection<Song>("Songs");
     }
 
+    [HttpPost]
+    public ActionResult<Song> Create(SongDTO songIn)
+    {
+        var song = new Song
+        {
+            Name = songIn.Name,
+            Artist = songIn.Artist,
+            Genre = songIn.Genre,
+            Chords = songIn.Chords
+        };
+        
+        _songs.InsertOne(song);  // Assuming _songs is your MongoDB collection
+        return CreatedAtRoute("GetSong", new { id = song.Id.ToString() }, song);
+    }
+
     // GET: api/Songs
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Song>>> GetSongs()
@@ -26,7 +41,7 @@ public class SongsController : ControllerBase
     }
 
     // GET: api/Songs/{id}
-    [HttpGet("{id}")]
+    [HttpGet("{id}", Name = "GetSong")]
     public async Task<ActionResult<Song>> GetSong(string id)
     {
         var song = await _songs.Find<Song>(song => song.Id == id).FirstOrDefaultAsync();
@@ -37,14 +52,6 @@ public class SongsController : ControllerBase
         }
 
         return Ok(song);
-    }
-
-    // POST: api/Songs
-    [HttpPost]
-    public async Task<ActionResult<Song>> PostSong([FromBody] Song song)
-    {
-        await _songs.InsertOneAsync(song);
-        return CreatedAtAction(nameof(GetSong), new { id = song.Id }, song);
     }
 
     // PUT: api/Songs/{id}
@@ -77,5 +84,36 @@ public class SongsController : ControllerBase
         await _songs.DeleteOneAsync(song => song.Id == id);
 
         return NoContent();
+    }
+
+    // GET: api/Songs/search/{query}
+    [HttpGet("search/{query}")]
+    public async Task<ActionResult<IEnumerable<Song>>> SearchSongs(string query)
+    {
+        var songs = await _songs.Find(song => 
+            song.Name.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+            song.Artist.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0 ||
+            song.Genre.IndexOf(query, StringComparison.OrdinalIgnoreCase) >= 0)
+            .ToListAsync();
+
+        return Ok(songs);
+    }
+
+    // POST: api/Songs/{id}/chords
+    [HttpPost("{id}/chords")]
+    public async Task<ActionResult<Song>> AddChord(string id, [FromBody] string chord)
+    {
+        var song = await _songs.Find<Song>(song => song.Id == id).FirstOrDefaultAsync();
+
+        if (song == null)
+        {
+            return NotFound();
+        }
+
+        song.Chords.Add(chord);
+
+        await _songs.ReplaceOneAsync(song => song.Id == id, song);
+
+        return Ok(song);
     }
 }
---
+++ SongbookAPI/SongDTO.cs
@@ -0,0 +1,7 @@
+public class SongDTO
+{
+    public string Name { get; set; }
+    public string Artist { get; set; }
+    public string Genre { get; set; }
+    public List<string> Chords { get; set; }
+}
